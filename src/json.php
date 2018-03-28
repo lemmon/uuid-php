04fbfd0a5973252277b4c13b6f68aae6959b891e
Lemmon <lemmon@users.noreply.github.com>
2018-03-28 18:18:45 +0200

    init

---
+++ src/json.php
@@ -0,0 +1,7 @@
+<?php
+
+function json($data) {
+  header('Content-Type: application/json');
+  echo json_encode($data);
+  exit;
+}
---
+++ src/view.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>UUID</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link rel="stylesheet" href="https://unpkg.com/abrusco@0.6.3/css/abrusco.min.css">
+</head>
+<body class="p1 sd-p2 col justify-center">
+
+  <div class="ac"><?= uuid() ?></div>
+
+</body>
+</html>
